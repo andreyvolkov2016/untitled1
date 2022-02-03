f1045795fc23d8d997e08c1d8bcd62ce3cac887f
Андрей <bekandr@mail.ru>
2022-02-03 17:54:39 +1000

    1

--- src/ru/mrcpk/java01/tasks/i01/t01/Main.java
+++ src/ru/mrcpk/java01/tasks/i01/t01/Main.java
@@ -2,6 +2,7 @@ package ru.mrcpk.java01.tasks.i01.t01;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
